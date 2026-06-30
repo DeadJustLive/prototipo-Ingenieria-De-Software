9511bb9852bc3024ff96e44a3c16d917330a5194
DeadJustLive <matiasretamalbarrera.45@gmail.com>
2026-06-30 19:55:06 -0400

    fix: usar rutas relativas

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: '/prototipo-Ingenieria-De-Software/',
+  base: './',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
